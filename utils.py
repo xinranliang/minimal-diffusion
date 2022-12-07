072a349f996f7726d1a7ba355299d727b82e83cc
Xinran Liang <xinranliang@princeton.edu>
2022-12-07 10:45:07 -0500

    sort some order

--- utils.py
+++ utils.py
@@ -23,12 +23,24 @@ def rgb_to_grayscale(image):
     return gray_image
 
 
-def test():
-    color_image = cv2.imread("/home/xinranliang/projects/minimal-diffusion/logs/cifar10_color/2022-12-01/samples/UNet_cifar10-1000_steps-250-sampling_steps-class_condn_False.png")
-    gray_image = rgb_to_grayscale(color_image)
-    cv2.imwrite("color.png", color_image)
-    cv2.imwrite("grayscale.png", gray_image)
-    return 
+class loss_logger:
+    def __init__(self, max_steps):
+        self.max_steps = max_steps
+        self.loss = []
+        self.start_time = time()
+        self.ema_loss = None
+        self.ema_w = 0.9
 
-if __name__ == "__main__":
-    test()
+    def log(self, v, display=True):
+        self.loss.append(v)
+        if self.ema_loss is None:
+            self.ema_loss = v
+        else:
+            self.ema_loss = self.ema_w * self.ema_loss + (1 - self.ema_w) * v
+
+        if display:
+            print(
+                f"Steps: {len(self.loss)}/{self.max_steps} \t loss (ema): {self.ema_loss:.3f} "
+                + f"\t Time elapsed: {(time() - self.start_time)/3600:.3f} hr"
+            )
+            
